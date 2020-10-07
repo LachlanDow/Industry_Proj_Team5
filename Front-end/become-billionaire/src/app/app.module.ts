0a311d927a9b2413b31e7f11c624fe56c8ab613d
Rauf Nawaz Tarar <33519174+raufnawaztarar@users.noreply.github.com>
2020-10-07 10:36:56 +0100

    #5 Created the main component, player screen

--- Front-end/become-billionaire/src/app/app.module.ts
+++ Front-end/become-billionaire/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {MatCardModule} from '@angular/material/card';
+import { HostSettingComponent } from './host-setting/host-setting.component'
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HostSettingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    // BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front-end/become-billionaire/src/app/host-setting/host-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host-setting',
+  templateUrl: './host-setting.component.html',
+  styleUrls: ['./host-setting.component.css']
+})
+export class HostSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
