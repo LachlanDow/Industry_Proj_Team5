5040a8469dc12717a9ff2d9b266fa0a626fd2b85
Robert <roberth1893@gmail.com>
2020-10-07 14:51:17 +0100

    Fixed question file commit mistake

--- Back-end/NodeAPI/models/Question.js
+++ Back-end/NodeAPI/models/Question.js
@@ -1,46 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const Question = require("../models/Question");
-
-// Get All Route
-router.get("/", async (req, res) => {
-    try {
-      const question = await Question.find()
-      res.json(question)
-    } catch (err) {
-      res.status(500).json({message: err.message})
-    }
-  });
-
-// Get One Route
-router.get("/:id", async (req, res) => {
-// Rest of the code will go here
-}); 
-
-// Create One Route
-router.post("/", async (req, res) => {
-    const question = new Question({
-      questionText: req.body.questionText,
-      answer: req.body.answer
-    });
-    try {
-      const newQuestion = await question.save();
-      res.status(201).json({ newQuestion });
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  });
-// Edit One Route PUT version
-router.put("/:id", async (req, res) => {
-// Rest of the code will go here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const questionSchema = new Schema({
+questionText: {
+  type: String,
+  required: true
+ },
+answer: {
+  type: String,
+  required: true
+ }
 });
-// Edit One Route PATCH version
-router.patch("/:id", async (req, res) => {
-// Rest of the code will go here
-});
-// Delete One Route
-router.delete("/:id", async (req, res) => {
-// Rest of the code will go here
-});
-
-module.exports = router;
+const Question = mongoose.model("Question", questionSchema);
+module.exports = Question;
