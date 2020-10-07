969d1c06706d0807ee86c446f0f1425bda2d0a96
LachlanDow <lachlan.dow@hotmail.co.uk>
2020-10-07 14:59:07 +0100

    Settings page development - NOT WORKING
    Settings page not working, uploaded for fixing

--- Front-end/become-billionaire/src/app/app.module.ts
+++ Front-end/become-billionaire/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import {MatCardModule} from '@angular/material/card';
-import { HostSettingComponent } from './host-setting/host-setting.component'
+import { HostSettingComponent } from './host-setting/host-setting.component';
+import { SettingsPageComponentComponent } from './settings-page-component/settings-page-component.component'
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    HostSettingComponent
+    HostSettingComponent,
+    SettingsPageComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front-end/become-billionaire/src/app/settings-page-component/settings-page-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-page-component',
+  templateUrl: './settings-page-component.component.html',
+  styleUrls: ['./settings-page-component.component.css']
+})
+export class SettingsPageComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
