027fd7e6dd283ac0853aa46a99720a55a937b010
Alek Roebuck <alekr56@gmail.com>
2020-10-07 22:02:28 +0100

    Added instructions screen
    Added placeholder instructions screen.
    Needs to be hooked up to the main screen button.

--- Front-end/become-billionaire/src/app/app.module.ts
+++ Front-end/become-billionaire/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import {MatCardModule} from '@angular/material/card';
-import { HostSettingComponent } from './host-setting/host-setting.component'
+import { HostSettingComponent } from './host-setting/host-setting.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    HostSettingComponent
+    HostSettingComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
