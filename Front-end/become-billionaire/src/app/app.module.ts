202871a0d589e742e4873522146ce0bae7268785
Rauf Nawaz Tarar <33519174+raufnawaztarar@users.noreply.github.com>
2020-10-08 12:25:45 +0100

    Fixed everything and linked instruction and settings compoent

--- Front-end/become-billionaire/src/app/app.module.ts
+++ Front-end/become-billionaire/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import {MatCardModule} from '@angular/material/card';
 import { HostSettingComponent } from './host-setting/host-setting.component';
 import { SettingsComponent } from './settings/settings.component'
@@ -12,9 +11,8 @@ import { InstructionsComponent } from './instructions/instructions.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
     HostSettingComponent,
-    SettingsComponent
+    SettingsComponent,
     InstructionsComponent
   ],
   imports: [
