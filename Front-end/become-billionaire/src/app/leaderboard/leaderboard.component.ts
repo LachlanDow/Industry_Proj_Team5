0c69a4aae040cbf8d31f9e4f96bd5ae20e893bfc
Matt Robb <robbmj@googlemail.com>
2020-10-09 19:25:06 +0100

    Adjusting position of the top players div

--- Front-end/become-billionaire/src/app/leaderboard/leaderboard.component.ts
+++ Front-end/become-billionaire/src/app/leaderboard/leaderboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LeaderboardComponent implements OnInit {
   display = false;
   
-  userScore = 76;
+  userScore = 75;
   constructor() { 
   }
 
