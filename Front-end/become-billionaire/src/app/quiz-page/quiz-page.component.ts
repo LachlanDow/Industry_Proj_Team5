59d2aad9c40d0a06ed1b7403acba691b348afcd2
Rauf Nawaz Tarar <33519174+raufnawaztarar@users.noreply.github.com>
2020-10-09 10:42:27 +0100

    Created UI for Quiz page and added cross/tick for answered questiones

--- Front-end/become-billionaire/src/app/quiz-page/quiz-page.component.ts
+++ Front-end/become-billionaire/src/app/quiz-page/quiz-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizPageQuizobject } from '../models/quiz-page.quizobject'
+import { Questions } from '../models/questions';
 
 @Component({
   selector: 'app-quiz-page',
@@ -7,84 +8,22 @@ import { QuizPageQuizobject } from '../models/quiz-page.quizobject'
   styleUrls: ['./quiz-page.component.css']
 })
 export class QuizPageComponent implements OnInit {
-  questions: QuizPageQuizobject[];
+  questions;
   currentQuestion = 0;
   quiz: QuizPageQuizobject[] = [];
   IsitCorrect;
+  correctAnswerIndex;
+  resultIcons = [] as boolean[];
+  questionNumber = 1;
 
   constructor() {
-    this.questions = [
-      {
-        "questionID": 1,
-        "question": "Eighteen thousandths, written as a decimal, is:",
-        "answers": [
-          "0.0018",
-          "0.018",
-          "0.18",
-          "1.80"
-        ],
-        "correctAnswer": 1
-      },
-      {
-        "questionID": 2,
-        "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
-        "answers": [
-          "12",
-          "13",
-          "14",
-          "15"
-        ],
-        "correctAnswer": 4
-      },
-      {
-        "questionID": 2,
-        "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
-        "answers": [
-          "12",
-          "13",
-          "14",
-          "15"
-        ],
-        "correctAnswer": 4
-      },
-      {
-        "questionID": 2,
-        "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
-        "answers": [
-          "12",
-          "13",
-          "14",
-          "15"
-        ],
-        "correctAnswer": 4
-      },
-      {
-        "questionID": 2,
-        "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
-        "answers": [
-          "12",
-          "13",
-          "14",
-          "15"
-        ],
-        "correctAnswer": 4
-      },
-      {
-        "questionID": 2,
-        "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
-        "answers": [
-          "12",
-          "13",
-          "14",
-          "15"
-        ],
-        "correctAnswer": 4
-      }
-    ]
+    
   }
 
 
   ngOnInit(): void {
+    this.questions = Questions.newQuiz.questions;
+    
   }
 
   // sortQuestions() {
@@ -95,17 +34,27 @@ export class QuizPageComponent implements OnInit {
   //   return this.quiz;
   // }
 
-  questionCount(e: any){
-    console.log(e);
-    if (e == this.questions[this.currentQuestion].correctAnswer) {
-      this.IsitCorrect = "Correct Answer! Well Done"
-      console.log("Correct Answer! Well Done");
+  questionCount(e: any) {
+    this.getCorrectAnswerIndex();
+    if (e == this.correctAnswerIndex) {
+      this.IsitCorrect = "Correct Answer! Well Done";
+      let icon = this.resultIcons.push(true)
+      console.log(this.resultIcons);
     }
     else {
-      console.log("Wrong Answer! Good luck next time");
-      this.IsitCorrect = "Wrong Answer! Good luck next time"
+      this.IsitCorrect = "Wrong Answer! Good luck next time";
+      // this.resultIcons.push(false);
+      console.log(this.resultIcons);
+      let icon = this.resultIcons.push(false)
+
     }
-    this.currentQuestion = this.currentQuestion+1;
+    this.currentQuestion = this.currentQuestion + 1;
+    this.questionNumber = this.questionNumber + 1;
+  }
+
+  getCorrectAnswerIndex() {
+    this.correctAnswerIndex = this.questions[this.currentQuestion].choices.findIndex(x =>x == this.questions[this.currentQuestion].answer);
+    this.correctAnswerIndex = this.correctAnswerIndex+1;
   }
 
 }
