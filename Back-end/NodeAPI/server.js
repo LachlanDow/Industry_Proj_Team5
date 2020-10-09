6472751ae70ee67e5813532bb514aef337a4bfc5
Robert <roberth1893@gmail.com>
2020-10-09 14:18:22 +0100

    Fixed cors placement issue

--- Back-end/NodeAPI/server.js
+++ Back-end/NodeAPI/server.js
@@ -11,6 +11,7 @@ const db = mongoose.connection;
 db.on("error", error => console.log(error));
 db.once("open", () => console.log("connection to db established"));
 app.use(express.json());
+app.use(cors());
 
 //Declaring routers for different routes
 const questionsRouter = require("./routes/Question");
@@ -19,7 +20,7 @@ const quizRouter = require("./routes/Quiz");
 app.use("/questions", questionsRouter);
 app.use("/quiz", quizRouter);
 app.use("/categories", quizRouter);
-app.use(cors());
+
 
 //App listen on specified port
 app.listen(3000, () => console.log('server has started at port 3000'));
