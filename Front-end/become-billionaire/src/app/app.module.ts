40302159acf9e1adc938eb08e70fd7e17d5aa1cc
Rauf Nawaz Tarar <33519174+raufnawaztarar@users.noreply.github.com>
2020-10-12 17:06:33 +0100

    #43 Render questions from API in frontend

--- Front-end/become-billionaire/src/app/app.module.ts
+++ Front-end/become-billionaire/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { HostSettingComponent } from './host-setting/host-setting.component';
 import { SettingsComponent } from './settings/settings.component'
 import { InstructionsComponent } from './instructions/instructions.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import {QuizPageComponent} from './quiz-page/quiz-page.component';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import {QuizPageComponent} from './quiz-page/quiz-page.component';
   imports: [
     BrowserModule,
     MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
