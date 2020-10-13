f05bed8f92fc1f7d136357a9db31836e9379a84c
Matt Robb <robbmj@googlemail.com>
2020-10-13 13:12:19 +0100

    score bar linked to configurable value variable.

--- Front-end/become-billionaire/src/app/leaderboard/leaderboard.component.ts
+++ Front-end/become-billionaire/src/app/leaderboard/leaderboard.component.ts
@@ -9,6 +9,12 @@ export class LeaderboardComponent implements OnInit {
   display = false;
   
   userScore = 76;
+  userScore1 = 86;
+  userScore2 = 82;
+  userScore3 = 78;
+  userScore4 = 72;
+  userScore5 = 68;
+  userScore6 = 64;
   constructor() { 
   }
 
