96270ec0b81a6b47ee32e27620b6bc91edf336ef
Robert <roberth1893@gmail.com>
2020-10-16 15:18:23 +0100

    Implemented join game lobby #45

--- Front-end/become-billionaire/src/app/join-game-lobby/join-game-lobby.component.ts
+++ Front-end/become-billionaire/src/app/join-game-lobby/join-game-lobby.component.ts
@@ -8,44 +8,15 @@ import { DataService } from '../services/data.service';
 })
 export class JoinGameLobbyComponent implements OnInit {
   participantID ;
-  localParticipants = [
-    {
-        "_id": "5f897b13985d7e4748970110",
-        "name": "username",
-        "score": 0
-    },
-    {
-        "_id": "5f897b35985d7e474897013b",
-        "name": "Tim",
-        "score": 0
-    },
-    {
-        "_id": "5f897bb9985d7e474897013d",
-        "name": "Rob",
-        "score": 0
-    },
-    {
-        "_id": "5f897bbd985d7e474897013f",
-        "name": "Matt",
-        "score": 0
-    },
-    {
-        "_id": "5f897bc9985d7e4748970141",
-        "name": "Mikhail",
-        "score": 0
-    },
-    {
-        "_id": "5f897bd2985d7e4748970143",
-        "name": "Bob",
-        "score": 0
-    }
-];
+  localParticipants;
+  quizID;
+  showQuizPage = false;
 
   constructor(private data: DataService) { }
 
   ngOnInit(): void {
     this.data.currentMessage.subscribe(message => this.participantID = message);
-   /**  this.getEvent(); */
+   this.getEvent();
   }
 
   /**
@@ -54,10 +25,17 @@ export class JoinGameLobbyComponent implements OnInit {
   getEvent() {
     let localQuiz;
     let quizPage = this;
-    console.log("sdfsdkfhj",this.participantID)
+    console.log(this.participantID);
     let serverEvents = new EventSource(`http://35.214.82.56:3000/stream/${this.participantID}`);
     serverEvents.addEventListener('message', function (event) {
       console.log(event.data);
+      let quiz = JSON.parse(event.data);
+      quizPage.quizID = quiz._id;
+      quizPage.localParticipants = quiz.participants;
+
+      if(quiz.questionNumber != 0) { 
+        quizPage.showQuizPage = true;
+      }
     });
 
   }
