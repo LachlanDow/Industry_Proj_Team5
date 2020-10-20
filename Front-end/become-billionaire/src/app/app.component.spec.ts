36d5241b8fddeb7f5dff59917929e74459a45029
LachlanDow <lachlan.dow@hotmail.co.uk>
2020-10-20 16:12:22 +0100

    #118 implemented tests for user input on main page

--- Front-end/become-billionaire/src/app/app.component.spec.ts
+++ Front-end/become-billionaire/src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let settingsButton: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,6 +16,9 @@ describe('AppComponent', () => {
       ],
       imports: [HttpClientModule],
     }).compileComponents();
+  
+
+  
   });
 
   it('should create the app', () => {
@@ -29,5 +38,41 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.title').textContent).toContain('BILLIONAIRE GOLF');
+  });
+
+  it('Pressing settings button turns displaysettings True', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.onSettingsPress();
+    expect(app.displaySettings).toBeTruthy();
+  });
+
+  it('Pressing join game button turns displayJoinPage to True', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.onJoinPress();
+    expect(app.displayJoinPage).toBeTruthy();
+  });
+  
+  it('Pressing start Game turns displayHost to True', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.onHostPress();
+    expect(app.displayHost).toBeTruthy();
+  });
+  
+  it('Pressing leaderboard button turns display leaderBoard True', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.onLeaderboardPress();
+    expect(app.displayLeaderboard).toBeTruthy();
+  });
+
+  
+  it('Pressing Instructions button turns displayInstructions True', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.onInstructionsPress();
+    expect(app.displayInstructions).toBeTruthy();
   });
 });
